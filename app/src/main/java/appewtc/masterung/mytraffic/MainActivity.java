7242ac4a21a1274f73898f486b12288495e40bc6
masterUNG <phrombutr@gmail.com>
2015-11-29 10:03:16 +0700

    สร้าง onClickListener เสร็จแว้วววว

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -2,6 +2,7 @@ package appewtc.masterung.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,10 +21,29 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
     }   // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }   //event
+        });
+
+    }
 
     private void bindWidget() {
 
-    }
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    }   // bindWidget
 
 }   // Main Class นี่คือคลาสหลัก นะคะ
