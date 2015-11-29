25f0e66bb2ce5d3481c669d92bd5d09eb005b9fa
masterUNG <phrombutr@gmail.com>
2015-11-29 12:56:07 +0700

    After Lunch

---
+++ app/src/main/java/appewtc/masterung/mytraffic/MyAdapter.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.mytraffic;
+
+import android.content.Context;
+
+/**
+ * Created by masterUNG on 11/29/15 AD.
+ */
+public class MyAdapter {
+
+    //Explicit
+    private Context objContext;
+
+}   // Main Class
